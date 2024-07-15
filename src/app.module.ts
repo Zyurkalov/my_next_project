dc26e9f48149ebc5c83dcb7f6d36f8c014c8a15c
Zyurkalov <Zyurkalov@gmail.com>
2024-07-15 23:41:19 +0500

    added config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
 import { OthersModule } from './others/others.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [OrdersModule, UsersModule, OthersModule],
+  imports: [OrdersModule, UsersModule, OthersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.PORT) | 5432,
+      username: process.env.USER,
+      password: process.env.PASWORD,
+      database: process.env.DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
